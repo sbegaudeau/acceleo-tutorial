95bd460ea3019df308f6ca3f9ffd2bf49920c740
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2012-06-11 11:36:58 +0200

    Build with feature and update site for the tutorial.

--- plugins/org.eclipse.acceleo.tutorial/src/org/eclipse/acceleo/tutorial/AcceleoProjectUnzipper.java
+++ plugins/org.eclipse.acceleo.tutorial/src/org/eclipse/acceleo/tutorial/AcceleoProjectUnzipper.java
@@ -43,7 +43,6 @@ public class AcceleoProjectUnzipper extends AbstractHandler {
 	 * 
 	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
 	 */
-	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		String parameter = event.getParameter("org.eclipse.acceleo.tutorial.projectUnzipperPath");
 		String path = "invalid";
